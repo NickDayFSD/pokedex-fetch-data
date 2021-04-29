3380b8fa6924cf32134e4828e5ab5ee9a5450518
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-04-29 09:16:08 -0700

    Fetch data from Pokedex API and log to console.

--- src/App/App.js
+++ src/App/App.js
@@ -7,7 +7,17 @@ import PokemonList from '../Pokemon/PokemonList';
 import PokemonSearch from '../Pokemon/PokemonSearch';
 import request from 'superagent';
 
+const POKEMON_API_URL = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
+
 class App extends Component {
+  state = {
+    pokemon: []
+  }
+
+  async componentDidMount() {
+    const response = await request.get(POKEMON_API_URL);
+    console.log(response.body);
+  }
 
   render() {
     return (
